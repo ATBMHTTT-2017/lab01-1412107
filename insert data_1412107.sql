0e74a8561c5e9e344aa88d60ee15cb1b2f6ec6a4
phandung1349 <phandung1349@gmail.com>
2017-05-08 10:17:43 +0700

    insert data done NHANVIEN pause  remaining tables, create security, insert data, phandung1349

---
+++ insert data_1412107.sql
@@ -0,0 +1,51 @@
+-------------------------------------3. insert data-------------------------------------------
+--1. PHONGBAN_1412107
+Insert into PHONGBAN_1412107 values ('PNS', N'Nhân S?', null, '22/May/1978', null, null);
+Insert into PHONGBAN_1412107 values ('PKH', N'K? Ho?ch', null, '01/Jan/1985', null, null);
+Insert into PHONGBAN_1412107 values ('PDA', N'?? Án', null, '19/Jun/1971', null, null);
+Insert into PHONGBAN_1412107 values ('PKD', N'Kinh Doanh', null, '23/Jul/1972', null, null);
+Insert into PHONGBAN_1412107 values ('PTT', N'Thông Tin', null, '23/Jul/1972', null, null);
+
+----2. NHANVIEN_1412107
+--------------1. truong duan
+Insert into NHANVIEN_1412107 values ('DANS001TR', N'?inh Bá Tiên', N'119 C?ng Qu?nh, Tp HCM', '0123456789', N't@g', null, null, 30000,  'DANS001TR');
+Insert into NHANVIEN_1412107 values ('DAKH001TR', N'Nguy?n Thanh Tùng', N'222 Nguy?n V?n C?, Tp HCM', '01343456789', N'tt@g', null, null, 23000,  'DAKH001TR');
+Insert into NHANVIEN_1412107 values ('DADA001TR', N'Tr?n Ng?c H?ng ', N'332 Nguy?n Thái H?c, Tp HCM', '01333456789', N'nh@g', null, null, 67000,  'DADA001TR');
+Insert into NHANVIEN_1412107 values ('DAKD001TR', N'Lê Qu?nh Nh?', N'291 H? V?n Huê, Tp HCM', '01333456679', N'n@g', null, null, 12000,  'DAKD001TR');
+Insert into NHANVIEN_1412107 values ('DATT001TR', N'Nguy?n Minh Hùng', N'95 Bà R?a, V?ng Tàu', '01333456129', N'mh@g', null, null, 55000,  'DATT001TR');
+--------------2. truong phong
+Insert into NHANVIEN_1412107 values ('NSTR', N'?inh Bá Quyên', N'119 Nguy?n C? Trinh, Tp HCM', '0345456789', N'bq@g', null, null, 52000,  'NSTR');
+Insert into NHANVIEN_1412107 values ('KHTR', N'Nguy?n Thái Vinh', N'222 Nguy?n Trãi, Tp HCM', '01673456789', N'tv@g', null, null, 41000,  'KHTR');
+Insert into NHANVIEN_1412107 values ('DATR', N'Tr?n H?i Xinh', N'332 Lê Lai, Tp HCM', '06423456789', N'hx@g', null, null, 36000,  'DATR');
+Insert into NHANVIEN_1412107 values ('KDTR', N'Lê H?i Tri?u', N'291 H? Hu?, Tp HCM', '0133347869', N'ht@g', null, null, 43000,  'KDTR');
+Insert into NHANVIEN_1412107 values ('TTTR', N'Nguy?n Ch??ng Hoàng', N'95 Bà R?a, V?ng Tàu', '01333434529', N'ch@g', null, null, 47000,  'TTTR');
+-------------3. truong chinhanh
+Insert into NHANVIEN_1412107 values ('CN001TR', N'Tr?n Cao Vân', N'119 Nguy?n C? Trinh, Tp HCM', '0345456789', N'cv@g', null, null, 52000,  'CN001TR');
+Insert into NHANVIEN_1412107 values ('CN002TR', N'Nguy?n Quy?n Linh', N'222 Nguy?n Trãi, Tp HCM', '01673456789', N'ql@g', null, null, 41000,  'CN002TR');
+Insert into NHANVIEN_1412107 values ('CN003TR', N'Tr?n Vi Linh', N'332 Lê Lai, Tp HCM', '06423456789', N'vl@g', null, null, 36000,  'CN003TR');
+Insert into NHANVIEN_1412107 values ('CN004TR', N'Võ Tr??ng Giang', N'291 H? Hu?, Tp HCM', '0133347869', N'tg@g', null, null, 43000,  'CN004TR');
+Insert into NHANVIEN_1412107 values ('CN005TR', N'Hoàng V?n Th?nh', N'95 Bà R?a, V?ng Tàu', '01333434529', N'vt@g', null, null, 47000,  'CN005TR');
+-------------4. nhanvien
+Insert into NHANVIEN_1412107 values ('NS001', N'Hoàng Thái Thành', N'119 Nguy?n C? Trinh, Tp HCM', '0345456789', N'tt@g', null, null, 52000,  'NS001');
+Insert into NHANVIEN_1412107 values ('KH001', N'Nguy?n V?n Linh', N'222 Nguy?n Trãi, Tp HCM', '01673456789', N'vl@g', null, null, 41000,  'KH001');
+Insert into NHANVIEN_1412107 values ('DA001', N'Tr?n Linh', N'332 Lê Lai, Tp HCM', '06423456789', N'tl@g', null, null, 36000,  'DA001');
+Insert into NHANVIEN_1412107 values ('KD001', N'Võ Tr??ng', N'291 H? Hu?, Tp HCM', '0133347869', N'vg@g', null, null, 43000,  'KD001');
+Insert into NHANVIEN_1412107 values ('TT001', N'Hoàng Th?nh', N'95 Bà R?a, V?ng Tàu', '01333434529', N'ht@g', null, null, 47000,  'TT001');
+--------------5. giam doc
+Insert into NHANVIEN_1412107 values ('GD001', N'Hoàng Thái', N'119 Nguy?n C? Trinh, Tp HCM', '0345456789', N'tt@g', null, null, 52000,  'GD001');
+Insert into NHANVIEN_1412107 values ('GD002', N'Nguy?n Xinh', N'222 Nguy?n Trãi, Tp HCM', '01673456789', N'nx@g', null, null, 41000,  'GD002');
+Insert into NHANVIEN_1412107 values ('GD003', N'Tr?n Phi', N'332 Lê Lai, Tp HCM', '06423456789', N'tp@g', null, null, 36000,  'GD003');
+Insert into NHANVIEN_1412107 values ('GD004', N'Võ H?i', N'291 H? Hu?, Tp HCM', '0133347869', N'vh@g', null, null, 43000,  'GD004');
+Insert into NHANVIEN_1412107 values ('GD005', N'Hoàng Chí', N'95 Bà R?a, V?ng Tàu', '01333434529', N'hc@g', null, null, 47000,  'GD005');
+
+
+
+--3. CHINHANH_1412107
+--4. DUAN_1412107
+Insert into DUAN_1412107 values ('DANS001', N'Nhân S?', null, '22/May/1978', null, null);
+Insert into PHONGBAN_1412107 values ('PKH', N'K? Ho?ch', null, '01/Jan/1985', null, null);
+Insert into PHONGBAN_1412107 values ('PDA', N'?? Án', null, '19/Jun/1971', null, null);
+Insert into PHONGBAN_1412107 values ('PKD', N'Kinh Doanh', null, '23/Jul/1972', null, null);
+Insert into PHONGBAN_1412107 values ('PTT', N'Thông Tin', null, '23/Jul/1972', null, null);
+--5. CHITIEU
+--6. PHANCONG
