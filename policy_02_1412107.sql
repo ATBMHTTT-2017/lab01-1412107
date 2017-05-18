602d366202f4eb338e4b78b8c40754694228afda
phandung1349 <phandung1349@gmail.com>
2017-05-18 15:29:40 +0700

    Update policy_02_1412107.sql

--- policy_02_1412107.sql
+++ policy_02_1412107.sql
@@ -2,7 +2,7 @@
 --connect as sys
 create role ROLE_GIAMDOC;
 --create view VIEW_DUAN
---connect ownerDBCONGTY
+--connect as sysadmin
 create view VIEW_DUAN as(
 select DUAN_1412107.maDA as mada , DUAN_1412107.tenDA as tenda, DUAN_1412107.kinhPhi as kinhphi, PHONGBAN_1412107.TENPHG as phongchutri
 , CHINHANH_1412107.tenCN as chinhanhchutri , NHANVIEN_1412107.HOTEN as truongduan, sum(DUAN_1412107.kinhPhi) as tongchi
@@ -12,10 +12,10 @@ and CHINHANH_1412107.maCN = NHANVIEN_1412107.chiNhanh
 group by (mada, tenda, kinhphi, phongchutri, chinhanhchutri, truongduan)
 );
 --grant select on VIEW_DUAN to role ROLE_GIAMDOC
---connect ownerDBCONGTY
+--connect as sysadmin
 grant select on VIEW_DUAN to ROLE_GIAMDOC;
 --grant role ROLE_GIAMDOC to GIAMDOC: GD001, GD002, GD003, GD004, GD005
---connect sys
+--connect as sys
 grant ROLE_GIAMDOC to GD001, GD002, GD003, GD004, GD005;
---connect as GD001, GD002, GD003, GD004, GD005
-select * from ownerDBCONGTY.VIEW_DUAN;
+--connect as GD001, GD002, GD003, GD004, GD005 to test
+select * from sysadmin.VIEW_DUAN;
