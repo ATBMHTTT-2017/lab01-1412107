1620e7e67358c11228616b948fa441893507cd23
phandung1349 <phandung1349@gmail.com>
2017-05-18 15:26:46 +0700

    Update policy_01_1412107.sql

--- policy_01_1412107.sql
+++ policy_01_1412107.sql
@@ -1,11 +1,10 @@
 --Chi truong phong duoc phep cap nhat va them thong tin vao du an
 --create role TRUONGPHONG
---connect sys
+--connect as sys
 create role ROLE_TRUONGPHONG;
---drop role ROLE_TRUONGPHONG;
---connect account: ownerDBCONGTY
+--connect as sysadmin
 grant select, insert, update on DUAN_1412107 to ROLE_TRUONGPHONG;
 --connect sys
 grant ROLE_TRUONGPHONG to NSTR, KHTR, DATR, KDTR, TTTR;
 --connect NSTR to test
-select * from ownerDBCONGTY.DUAN_1412107;
+select * from sysadmin.DUAN_1412107;
